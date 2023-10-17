a7f1a9371a1d6f630f7691fa2e82c4a902cbfe99
Andrengineer <ndrengineer@gmail.com>
2023-10-17 15:00:27 -0500

    Fix Image

--- components/header/Navigation.vue
+++ components/header/Navigation.vue
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container">
             <NuxtLink class="navbar-brand green" to="/">
-                <img src="~/assets/images/logo.png" alt="Andrengineer" width="30">ndrengineer
+                <img src="/images/logo.png" alt="Andrengineer" width="30">ndrengineer
             </NuxtLink>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
