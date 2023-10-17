4804847a0214ca45730b0bd90f4c71287ac8c21f
Andrengineer <ndrengineer@gmail.com>
2023-10-17 12:44:22 -0500

    Style Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,10 +8,18 @@ export default defineNuxtConfig({
       },
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
+      link: [
+        {rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css'},
+        {rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css'},
+      ],
+      script: [
+        {src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js', tagPosition: 'bodyClose'}
+      ]
     },
     pageTransition: { 
       name: 'page', 
       mode: 'out-in' 
     }
-  }
+  },
+  css: ['~/assets/css/default.css']
 })
